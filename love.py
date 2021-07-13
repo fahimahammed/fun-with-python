aa45525356425bfacdb74d48de8a42afe967506f
fahimahammed <fahimahammmed.cse@gmail.com>
2021-07-13 23:43:21 +0600

    draw star with python

---
+++ love.py
@@ -0,0 +1,18 @@
+from turtle import *
+import turtle
+import time
+
+color("red")
+begin_fill()
+left(50)
+forward(100)
+circle(40, 180)
+left(260)
+circle(40, 180)
+forward(100)
+end_fill()
+time.sleep(2)
+turtle.clear()
+turtle.write(" Love you Chumki", font=("Verdana", 25, "normal"), align= "center")
+done()
+
---
+++ star.py
@@ -0,0 +1,20 @@
+from turtle import *
+import time
+
+def star():
+    for i in range(0, 5):
+        forward(100)
+        right(144)
+    left(144)
+    forward(100)
+    
+
+star()
+star()
+star()
+star()
+star()
+
+time.sleep(2)
+
+done()
